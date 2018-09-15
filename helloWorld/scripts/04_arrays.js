2d2c7b24f4c2a55616551eb3eb27ebc8e5312f30
maritimeseamanship <ekaterinataer@gmail.com>
2018-09-15 20:08:04 +0300

    Added Drivers

--- helloWorld/scripts/04_arrays.js
+++ helloWorld/scripts/04_arrays.js
@@ -65,4 +65,48 @@ document.addEventListener('DOMContentLoaded', function(){
     console.log(animal, katya, phoneBook);
     phoneBook[0].phone = '121212';
     console.log(animal, katya, phoneBook);
+
+   let drivers = [
+    {
+        name: 'Sam',
+        phone: '151515',
+        adress: {
+            city: 'Rivne',
+            street: 'Soborna'
+        }
+    },
+    {
+        name: 'John',
+        phone: '565656',
+        adress: {
+            city: 'Kyiv',
+            street: 'Solomon'
+        }
+    },
+    {
+        name: 'Sasha',
+        phone: '464646',
+        adress: {
+            city: 'Ternopil',
+            street: 'Sheva'
+        }
+    },
+    {
+        name: 'Lida',
+        phone: '474747',
+        adress: {
+            city: 'Rivne',
+            street: 'Olga'
+        }
+    },
+    {
+        name: 'David',
+        phone: '787878',
+        adress: {
+            city: 'Kyiv',
+            street: 'Fuck'
+        }
+    }
+   ]
 });
+
