d803c2a963e3693e2f639f5ae2afe11ea44ac75c
maritimeseamanship <ekaterinataer@gmail.com>
2018-09-17 21:28:19 +0300

    Conditional in progress

---
+++ helloWorld/scripts/05_ifElse.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function(){
+    let user = {
+        name: null,
+        id: null
+    };
+
+    if (user.id === null) {
+        console.log('user doesnt exist');
+    }
+});
