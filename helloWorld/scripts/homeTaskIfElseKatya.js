1e8e4cbe9dfccc44b7b98f6c8c1b1c9f6c533c84
dewona <dewona.ig@gmail.com>
2018-09-24 18:01:15 +0300

    another way home task

--- helloWorld/scripts/homeTaskIfElseKatya.js
+++ helloWorld/scripts/homeTaskIfElseKatya.js
@@ -1,14 +1,46 @@
 document.addEventListener('DOMContentLoaded', function(){
     let object = {
-        name: 'Aloy',
-        password: '684339',
+        name: 'Al',
+        password: '',
         age: 18
     };
-    if (object.name.length > 2 && object.password.length >=3 && object.password.length <=8 && object.age >= 18 && object.age != isNaN()) {
+    
+    // if (object.name.length > 2 && object.password.length >=3 && object.password.length <=8 && object.age >= 18 && object.age != isNaN()) {
+    //    console.log('validUser');
+    // }
+    // else {
+    //     console.log('inValid');
+    // }
 
-       console.log('validUser');
+    let isValid = true;
+    let invalidMessage = [];
+
+    if (object.name.length <= 2) {
+        isValid = false;
+        invalidMessage.push('User name too short');
     }
-    else {
-        console.log('inValid');
+
+    if (object.password.length < 2){
+        isValid = false;
+        invalidMessage.push('Password name too short');
     }
+
+    if (object.password.length > 8){
+        isValid = false;
+        invalidMessage.push('Password name too long');
+    }
+
+    if (object.age < 18){
+        isValid = false;
+        invalidMessage.push('You are too young');
+    }
+
+    if (isNaN(object.age)){
+        isValid = false;
+        invalidMessage.push('Prompt number!');
+    }
+
+    console.log('isValid: ', isValid);
+    console.log(invalidMessage);
+
 });
